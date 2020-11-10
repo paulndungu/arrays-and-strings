554a1b1635f2cc0f895d08ee6204a3364914d194
paulndungu <p.ndungu.kara@gmail.com>
2020-11-10 13:33:06 +0300

    finished up with the code

---
+++ src/set of and mutable set of.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    var intset= setOf(1,2,3,4,5)
+    println("all inset elements: $intset")
+
+    var minset= mutableSetOf(1,2,3,4,5)
+    minset.add(6)
+    println("all minset elements: $minset")
+
+}
